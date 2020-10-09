6ee9a84c190f7088ef473b877bcd24df12ebabad
DravitKumar <dravitkumar@vm.mshome.net>
2020-10-09 18:57:32 +0530

    useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div>
-      Hello World
+    <div className="counters">
+      <p>You clicked {count} times.</p>
+      <button onClick={() => setCount(count+1)}>Increment</button>
+      <button onClick={() => setCount(count-1)}>Decrement</button>
     </div>
   );
 }
